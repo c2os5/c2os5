bdb51bfc43462a7516114b95345605094eeb4a6e
ccckmit <ccckmit@gmail.com>
2023-05-27 04:41:54 +0000

    04b pass

--- mini-riscv-os/04b-TimerInterrupt/os.c
+++ mini-riscv-os/04b-TimerInterrupt/os.c
@@ -4,7 +4,10 @@ int os_main(void)
 {
 	lib_puts("OS start\n");
 	timer_init(); // start timer interrupt ...
-	while (1) {} // os : do nothing, just loop!
+	while (1) {
+            lib_puts("os_main:loop\n");
+            lib_delay(500);
+        } // os : do nothing, just loop!
 	return 0;
 }
 
