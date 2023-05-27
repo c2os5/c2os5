be22c6b4f3757fd84aad1d479e2640d1cd996e79
ccckmit <ccckmit@gmail.com>
2023-05-27 12:35:43 +0800

    add mini-riscv-os/04b

---
+++ mini-riscv-os/04b-TimerInterrupt/task.h
@@ -0,0 +1,17 @@
+#ifndef __TASK_H__
+#define __TASK_H__
+
+#include "riscv.h"
+#include "sys.h"
+
+#define MAX_TASK 10
+#define STACK_SIZE 1024
+
+extern int taskTop;
+extern struct context ctx_os;
+
+extern int  task_create(void (*task)(void));
+extern void task_go(int i);
+extern void task_os();
+
+#endif
